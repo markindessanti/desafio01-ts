8d66381e85bafad6b0f4498df2f8d5e61bcff6c7
markindessanti <markindessanti@gmail.com>
2023-08-10 16:06:58 -0300

    Commit Inicial

---
+++ class/CompanyAccount.ts
@@ -0,0 +1,11 @@
+import { DioAccount } from "./DioAccount";
+
+export class CompanyAccount extends DioAccount {
+	constructor(name: string, accountNumber: number) {
+		super(name, accountNumber);
+	}
+
+	getLoan = (): void => {
+		console.log("Voce pegou um empréstimo");
+	};
+}
